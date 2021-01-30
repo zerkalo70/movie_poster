35980b8589cfbe2ab22333f1c2a2f9f97d961499
Сергей <halygali@mail.ru>
2021-01-30 09:31:49 +0300

    project complite

---
+++ src/layout/Header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+    return <nav className="#43a047 green darken-1">
+        <div className="nav-wrapper">
+            <a href="#" className="brand-logo">Movie Poster</a>
+            <ul id="nav-mobile" className="right hide-on-med-and-down">
+                <li><a href="!#">Repo</a></li>
+            </ul>
+        </div>
+    </nav>
+}
+
+export {Header}
